08a44888af391d2759524eed9899b1e5fe7bc87c
Buddy <>
2025-05-20 06:13:30 +0800

    feat: add API and parameter annotations

---
+++ spring-siyukio-tools/src/main/java/com/siyukio/tools/api/annotation/ApiController.java
@@ -0,0 +1,49 @@
+package com.siyukio.tools.api.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.lang.annotation.*;
+
+/**
+ * Api controller
+ *
+ * @author Buddy
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Controller
+@RequestMapping(method = {RequestMethod.POST})
+public @interface ApiController {
+
+    /**
+     * Alias for {@link Controller#value}.
+     */
+    @AliasFor(annotation = Controller.class)
+    String value() default "";
+
+    /**
+     * Alias for {@link RequestMapping#path}.
+     */
+    @AliasFor(annotation = RequestMapping.class)
+    String[] path() default {};
+
+    /**
+     * Category tags.
+     */
+    String[] tags();
+
+    /**
+     * Whether it can be used as an MCP service tool, default is true.
+     */
+    boolean mcpTools() default true;
+
+    /**
+     * Summary
+     */
+    String summary();
+
+}
