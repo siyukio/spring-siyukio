48ab9ea175e0a68b447c2ef6e5155e2b01e91857
Buddy <>
2025-06-02 19:24:23 +0800

    feat: implement automatic token injection for API calls

---
+++ spring-siyukio-application/src/main/java/com/siyukio/application/method/TokenArgumentResolver.java
@@ -0,0 +1,28 @@
+package com.siyukio.application.method;
+
+import com.siyukio.tools.api.constants.ApiConstants;
+import com.siyukio.tools.api.token.Token;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * @author Buddy
+ */
+public final class TokenArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(Token.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        return request.getAttribute(ApiConstants.ATTRIBUTE_TOKEN);
+    }
+
+}
