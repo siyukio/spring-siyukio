946532d74cc8afe4fee3b929058c0c6a4864da5d
Buddy <>
2025-06-02 09:52:14 +0800

    feat: add KeyUtils for generating and reading PEM keys

---
+++ spring-siyukio-tools/src/test/java/com/siyukio/tools/util/KeyUtilsTest.java
@@ -0,0 +1,14 @@
+package com.siyukio.tools.util;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Buddy
+ */
+public class KeyUtilsTest {
+
+    @Test
+    void testCreateKeyPair() throws Exception {
+        KeyUtils.createKeyPair();
+    }
+}
