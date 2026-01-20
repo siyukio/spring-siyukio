d638db4f544cf8eed49954c1d382fd001b2b2321
Bugee <>
2026-01-20 16:53:14 +0800

    refactor: simplify ApiClient annotation by using HttpExchange

--- spring-siyukio-tools/src/main/java/io/github/siyukio/tools/api/annotation/client/ApiClient.java
+++ spring-siyukio-tools/src/main/java/io/github/siyukio/tools/api/annotation/client/ApiClient.java
@@ -1,6 +1,10 @@
 package io.github.siyukio.tools.api.annotation.client;
 
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.service.annotation.HttpExchange;
+
 import java.lang.annotation.*;
+import java.net.http.HttpClient;
 
 /**
  * Api client.
@@ -10,64 +14,39 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@HttpExchange
 public @interface ApiClient {
 
     /**
-     * Base URLs should always end in /
-     * Base URL: http://example.com/api/ Endpoint: foo/bar/ Target: http://example.com/api/foo/bar/
-     * Base URL: http://example.com/api/ Endpoint: /foo/bar/ Target: http://example.com/foo/bar/
+     * Base URL for the API.
      */
-    String baseUrl() default "";
+    @AliasFor(annotation = HttpExchange.class)
+    String url();
 
-    ApiClientHeader[] headers() default {};
+    @AliasFor(annotation = HttpExchange.class)
+    String method() default "";
+
+    @AliasFor(annotation = HttpExchange.class)
+    String contentType() default "";
+
+    @AliasFor(annotation = HttpExchange.class)
+    String[] accept() default {};
+
+    @AliasFor(annotation = HttpExchange.class)
+    String[] headers() default {};
 
     /**
-     * HTTP connection timeout, default is 10 seconds.
+     * HTTP connection timeout, default is 6 seconds.
      */
     int connectTimeout() default 6;
 
     /**
-     * HTTP read timeout, default is 10 seconds.
+     * HTTP read timeout, default is 60 seconds.
      */
-    int readTimeout() default 10;
+    int readTimeout() default 60;
 
     /**
-     * HTTP write timeout, default is 10 seconds.
+     * HTTP version, default is HTTP/2.
      */
-    int writeTimeout() default 10;
-
-    /**
-     * Maximum number of idle connections in the HTTP connection pool, default is 60.
-     */
-    int maxIdleConnections() default 60;
-
-    /**
-     * Idle connection eviction time in the HTTP connection pool, default is 15 minutes.
-     */
-    int keepAliveDuration() default 15;
-
-    /**
-     * Maximum number of concurrent requests, default is 60.
-     */
-    int maxRequests() default 60;
-
-    /**
-     * Maximum number of concurrent requests per host, default is 45.
-     */
-    int maxRequestsPerHost() default 45;
-
-    /**
-     * Whether to use the h2c protocol (HTTP/2 cleartext), default is false.
-     */
-    boolean h2c() default false;
-
-    /**
-     * Whether to force the use of HTTP/1, default is false.
-     */
-    boolean http1Only() default false;
-
-    /**
-     * Whether to automatically generate an app role token during the request.
-     */
-    boolean autoAppToken() default false;
+    HttpClient.Version version() default HttpClient.Version.HTTP_2;
 }
